3b2053f44cd33c76faa8b0efbc504a542a686960
Matrixoid <40433531+Matrixoid@users.noreply.github.com>
2022-01-29 18:01:19 +0300

    Update T6.hs

--- hw0/src/HW0/T6.hs
+++ hw0/src/HW0/T6.hs
@@ -6,3 +6,7 @@ import Data.Char (isSpace)
 a = distrib (Left ("AB" ++ "CD" ++ "EF"))     -- distrib from HW0.T1
 b = map isSpace "Hello, World"
 c = if 1 > 0 || error "X" then "Y" else "Z"
+
+a_whnf = (Left ("AB" ++ "CD" ++ "EF"), Left ("AB" ++ "CD" ++ "EF"))
+b_whnf = [isSpace 'H', isSpace 'e', isSpace 'l', isSpace 'l', isSpace 'o', isSpace ',', isSpace ' ', isSpace 'W', isSpace 'o', isSpace 'r', isSpace 'l', isSpace 'd']
+c_whnf = "Y"
